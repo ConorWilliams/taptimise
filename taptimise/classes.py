f0c17cf3e1952a4fc97ffbfa322186653a0edd74
ConorWilliams <conorwilliams@outlook.com>
2019-09-18 00:15:27 +0100

    implement tap class

--- taptimise/classes.py
+++ taptimise/classes.py
@@ -2,6 +2,9 @@
 
 import random
 
+DISTANCE_PENALTY = 10
+OVERLOAD_EXPONENT = 2
+
 
 class Buffer():
     def __init__(self, size):
@@ -20,30 +23,62 @@ class Buffer():
             self.data[pos] = obj
             pos += 1
 
-    def rand(self)
+    def rand(self):
         return self.data[random.randint(0, len(self.data))]
 
 
 class Tap():
-    def __init__(self):
-        pass
+    def __init__(self, max_load, exp_load):
+        self.pos = complex(0, 0)
+        self.vec_sum = complex(0, 0)
+
+        self.energy = 0
+        self.old_energy = 0
+
+        self.load = 0
+        self.max_load = max_load
+        self.exp_load = exp_load
+
+        self.houses = set()
 
     def centralise(self):
-        pass
+        self.pos = self.vec_sum / self.load
 
     def score(self):
-        pass
+        self.old_energy = self.energy
+        self.energy = 0
+
+        for h in self.houses:
+            self.energy += bond_energy(self, h)
+
+        if self.load > self.max_load:
+            energy *= (self.load / max_load) ** OVERLOAD_EXPONENT
+
+        energy *= ((self.load / self.max_load - exp_load) / exp_load)**2
 
 
 class House():
-    def __init__(self, x, y, demand, buff_size):
+    def __init__(self, x, y, demand, buff_size, max_sq_dist):
         self.pos = complex(x, y)
         self.demand = demand
         self.tap = None
         self.buff = Buffer(buff_size)
+        self.max_sq_dist = max_sq_dist
 
     def attach(self, tap):
         pass
 
-    def detach(self)
+    def detach(self):
         pass
+
+
+def bond_energy(tap, house):
+    rel = tap.pos - house.pos
+    rel = rel.real**2 + rel.imag**2
+
+    if rel > house.max_sq_dist:
+        rel *= DISTANCE_PENALTY
+
+    rel *= house.demand
+
+    return rel
--- taptimise/optimise.py
+++ taptimise/optimise.py
@@ -5,6 +5,8 @@ from .classes import Tap, House
 
 def optimise(houses, num_taps, max_load, steps=None, debug=False, multiscale=True, max_dist=-1):
 
+    a, b = Tap(1, 2), House(1, 2, 3, 4, 5)
+
     h_out, t_out, max_dist, debug_data = 1, [], 3, 4
 
     return h_out, t_out, max_dist, debug_data
